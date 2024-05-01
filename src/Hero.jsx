5d51a2f630c7a187db43869d3fa295a5be237249
Neha <neharadialcode@gmail.com>
2024-05-01 10:08:01 +0530

    first commit

---
+++ src/Hero.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Hero = () => {
+  return (
+    <div>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit quia
+      fugiat debitis vel molestiae veniam sint officiis eum! Similique et dolore
+      qui dolorem architecto aut, eveniet magnam maxime incidunt iste?
+    </div>
+  );
+};
+
+export default Hero;
